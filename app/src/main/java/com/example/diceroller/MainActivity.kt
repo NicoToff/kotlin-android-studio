b810009f705588ab2407ee6e48e225235ecf8c13
NicoToff <toffolo01@hotmail.com>
2022-12-08 17:11:15 +0100

    Finished app

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollResult = findViewById<TextView>(R.id.rollResult)
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val diceFaceInput = findViewById<TextView>(R.id.inputDiceFaces)
+
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
+            var str = diceFaceInput.text.toString();
+            if(str == "") str = "0"
+            var faces = Integer.parseInt(str)
+            if(faces < 2) {
+                faces = 2
+                diceFaceInput.text = faces.toString()
+            } else if (faces > 100) {
+                faces = 100
+                diceFaceInput.text = faces.toString()
+            }
+            val die = Dice(faces)
+            rollResult.text = die.roll().toString()
+        }
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
