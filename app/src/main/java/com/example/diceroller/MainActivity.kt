83a07c8eb27a82903cbc12a1d97f981a8b5023c4
NicoToff <toffolo01@hotmail.com>
2022-12-09 09:40:03 +0100

    Removed unnecessary code ; added dice images as ressources

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.diceroller
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollResult = findViewById<TextView>(R.id.rollResult)
         val rollButton = findViewById<Button>(R.id.rollButton)
 
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
 
             val result = Dice(6).roll()
-            rollResult.text = result.toString()
-//            when {
+//            when (result) {
 //
 //            }
         }
