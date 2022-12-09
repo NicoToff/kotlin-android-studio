47dd83f3ce8a89f9ed1eeebe983f9df6e13a7663
NicoToff <toffolo01@hotmail.com>
2022-12-09 09:29:19 +0100

    Initial commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,29 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         val rollResult = findViewById<TextView>(R.id.rollResult)
         val rollButton = findViewById<Button>(R.id.rollButton)
-        val diceFaceInput = findViewById<TextView>(R.id.inputDiceFaces)
-        val textResult = findViewById<TextView>(R.id.textResult)
 
         rollButton.setOnClickListener {
             Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
-            var str = diceFaceInput.text.toString();
-            if(str == "") str = "0"
-            var faces = Integer.parseInt(str)
-            if(faces < 2) {
-                faces = 2
-                diceFaceInput.text = faces.toString()
-            } else if (faces > 100) {
-                faces = 100
-                diceFaceInput.text = faces.toString()
-            }
-            val die = Dice(faces)
-            val result = die.roll()
+
+            val result = Dice(6).roll()
             rollResult.text = result.toString()
-            when {
-                result == faces -> textResult.text = "Max roll, babaaayy!"
-                result == 1 && faces > 5 -> textResult.text = "Oooof, fumble..."
-                else -> textResult.text = ""
-            }
+//            when {
+//
+//            }
         }
     }
 }
